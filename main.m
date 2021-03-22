a433b19d9994af65587a6e45af741160fc81c8d7
nolanflynn <nolanflynn0@gmail.com>
2021-03-21 23:28:43 -0400

    Whole bunch of simulink

--- main.m
+++ main.m
@@ -3,7 +3,12 @@ close all
 clear all
 format compact
 
-[wn, l_eff] = gyroscope_parameters()
-[K, tau] = motor_parameters()
+[wn, l_eff] = gyroscope_parameters();
+[K_motor, tau] = motor_parameters();
+
+g = 9.81;
+[Kp, Ki] = control_parameters(K_motor, tau, wn, l_eff)
+% Kp = -Kp
+% Ki = -Ki
 
 % avg_vel = control_system
